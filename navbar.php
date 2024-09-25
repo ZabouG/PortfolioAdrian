3ba4290d95d214c1956c020b84dd02e888871e44
ZabouG <160099549+ZabouG@users.noreply.github.com>
2024-09-25 20:49:43 +0400

    NavBar

---
+++ navbar.php
@@ -0,0 +1,9 @@
+<!-- navbar.php -->
+<nav class="navbar">
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="about.php">À Propos</a></li>
+        <li><a href="portfolio.php">Portfolio</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
